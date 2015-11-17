83d4cf5af1d402ae4b32ad493b201d3b2834d190
Simon dePonf <simon.depontfarcy@gmail.com>
2015-11-17 17:27:29 +0100

    CRUD completed
    #15
    lister les utilisateurs
    créer un utilisateur
    modifier un utilisateur
    supprimer un utilisateur

--- src/main/java/dta/pizzeria/backend/rest/UtilisateurResource.java
+++ src/main/java/dta/pizzeria/backend/rest/UtilisateurResource.java
@@ -9,6 +9,9 @@ import dta.pizzeria.backend.entity.Utilisateur;
 import dta.pizzeria.backend.metier.UtilisateurService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +21,36 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ETY01
  */
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/user")
 public class UtilisateurResource {
     
-    @Autowired UtilisateurService userService;
+    @Autowired
+    UtilisateurService userService;
     
-    @RequestMapping(method = RequestMethod.GET ,value="/")
-    public List<Utilisateur> listDrinks() {
-            return userService.findAll();
+    @RequestMapping(method = RequestMethod.GET )
+    public List<Utilisateur> listUsers() {
+        return userService.findAll();
     }
+    
+    @RequestMapping(method = RequestMethod.GET ,value="/{id}")
+    public Utilisateur getUser(@PathVariable("id") Long id) {
+        return userService.findOne(id);
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public void saveUser(@RequestBody Utilisateur user) {
+        userService.save(user);
+    }
+    
+    @RequestMapping(method = RequestMethod.PUT)
+    public void updateUser(@RequestBody Utilisateur user) {
+        userService.save(user);
+    }
+    
+    @RequestMapping(method = RequestMethod.DELETE,value="/{id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        userService.delete(id);
+    }
+    
 }
