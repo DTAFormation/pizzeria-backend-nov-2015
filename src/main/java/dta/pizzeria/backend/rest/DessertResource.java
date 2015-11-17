a33695286ed2fc12d6cf5dadc7ab9f366ea86319
valoo86 <valentinmatignon@gmail.com>
2015-11-17 10:22:32 +0100

    retrait de méthodes non testées

--- src/main/java/dta/pizzeria/backend/rest/DessertResource.java
+++ src/main/java/dta/pizzeria/backend/rest/DessertResource.java
@@ -3,10 +3,8 @@ package dta.pizzeria.backend.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dta.pizzeria.backend.entity.Produits;
@@ -21,25 +19,5 @@ public class DessertResource {
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Produits> listDesserts() {
 		return produitsService.listDesserts();
-	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	public void setDessert(@ModelAttribute("dessert") Produits dessert){
-		produitsService.setProduits(dessert);
-	}
-
-	@RequestMapping(method = RequestMethod.PUT)
-	public void updateDessert(@ModelAttribute("dessert") Produits dessert){
-		produitsService.updateProduits(dessert);
-	}
-
-	@RequestMapping(method = RequestMethod.DELETE, value="/{id}")
-	public void removeDessert(@RequestParam long id){
-		produitsService.removeProduits(id);
-	}
-	
-	@RequestMapping(method = RequestMethod.GET, value="/{id}")
-	public Produits getDessert(@RequestParam long id){
-		return produitsService.getProduits(id);
 	}
 }
