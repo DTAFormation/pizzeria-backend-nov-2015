54a30f41a1c48ebade825df1bb1607f331208b39
Vincent LANDRA <gvince958@gmail.com>
2015-11-18 13:54:06 +0100

    Refonte Backend

--- src/main/java/dta/pizzeria/backend/rest/CommandeResource.java
+++ src/main/java/dta/pizzeria/backend/rest/CommandeResource.java
@@ -19,54 +19,63 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class CommandeResource {
+
     @Autowired
     private CommandeService commandeService;
-    
-    @RequestMapping(value="/command", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/commande", method = RequestMethod.GET)
     public List<Commande> listCommandes() {
         return commandeService.findAll();
     }
-    
-    @RequestMapping(value="/commandesPretes", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/commande", method = RequestMethod.POST)
+    public Commande setCommande(@RequestBody Commande commande) {
+        return commandeService.save(commande);
+    }
+
+    @RequestMapping(value = "/commandesPretes", method = RequestMethod.GET)
     public List<Commande> listCommandesPretes() {
         return commandeService.findByEtat(Commande.Etat.PREPARE);
     }
-    
-    @RequestMapping(value="/commandesPretesCaisse", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/commandesPretesCaisse", method = RequestMethod.GET)
     public List<Commande> listCommandesPretesCaisse() {
         List<Commande> retour = new ArrayList<>();
         List<Commande> enCours = commandeService.findByEtat(Commande.Etat.PREPARE);
-        for(Commande commande:enCours){
-            if(!commande.getType().equals(Commande.Type.LIVRAISON))
+        for (Commande commande : enCours) {
+            if (!commande.getType().equals(Commande.Type.LIVRAISON)) {
                 retour.add(commande);
+            }
         }
-        
+
         return retour;
     }
-    
-    @RequestMapping(value="/commandesPretesLivraison", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/commandesPretesLivraison", method = RequestMethod.GET)
     public List<Commande> listCommandesPretesLivraison() {
         List<Commande> retour = new ArrayList<>();
         List<Commande> enCours = commandeService.findByEtat(Commande.Etat.PREPARE);
-        for(Commande commande:enCours){
-            if(commande.getType().equals(Commande.Type.LIVRAISON))
+        for (Commande commande : enCours) {
+            if (commande.getType().equals(Commande.Type.LIVRAISON)) {
                 retour.add(commande);
+            }
         }
         List<Commande> enLivraison = commandeService.findByEtat(Commande.Etat.LIVRAISON);
-        for(Commande commande:enLivraison){
-            if(commande.getType().equals(Commande.Type.LIVRAISON))
+        for (Commande commande : enLivraison) {
+            if (commande.getType().equals(Commande.Type.LIVRAISON)) {
                 retour.add(commande);
+            }
         }
-        
+
         return retour;
     }
-    
-    @RequestMapping(value="/commandesEnCours", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/commandesEnCours", method = RequestMethod.GET)
     public List<Commande> listCommandesEnCours() {
         return commandeService.findByEtat(Commande.Etat.EN_COURS);
     }
-    
-    @RequestMapping(value="/command", method = RequestMethod.PUT)
+
+    @RequestMapping(value = "/command", method = RequestMethod.PUT)
     public void updateCommandes(@RequestBody Commande commande) {
         commandeService.save(commande);
     }
